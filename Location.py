c7f4e7a9042386901a0e6e19f5d4c42e1a77a756
XST <dadelaricaar@gmail.com>
2016-07-24 01:57:08 +0200

    Added new class

--- Location.py
+++ Location.py
@@ -19,3 +19,22 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+import utils
+
+class Location(object):
+
+    def __init__(self, latitude = 0, longitude = 0):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def distance(self, otherLocation):
+       return utils.haversine(self, otherLocation)
+
+    def noise(self, noise):
+        latitude = self.latitude + utils.randomNoise(noise)
+        longitude = self.longitude + utils.randomNoise(noise)
+
+        return Location(latitude, longitude)
+
+
--- Utils.py
+++ Utils.py
@@ -19,3 +19,60 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+from math import radians, cos, sin, asin, sqrt
+from random import randint
+import simplekml
+import csv
+
+def haversine(location1, location2):
+    lon1 = location1.longitude
+    lat1 = location1.latitude
+
+    lon2 = location2.longitude
+    lat2 = location2.latitude
+
+    # Convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # Haversine formula
+    dLon = lon2 - lon1
+    dLat = lat2 - lat1
+    a = sin(dLat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dLon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371000  # Radius of earth in meters. Use 3956 for miles
+    return c * r
+
+
+def randomNoise(noise):
+    if randint(0, 2):
+        z = -1
+    else:
+        z = 1
+
+    rNoise = (randint(0, noise) / 1000000.0) * z
+
+    return rNoise
+
+def exportXml(locations, filePath = "export.kml"):
+    kml = simplekml.Kml()
+
+    for x in xrange(len(locations)):
+        location = locations[x]
+        kml.newpoint(name=str(x),coords=[(location.longitude,location.latitude)])
+
+    #print(kml.kml())
+    kml.save(filePath)
+
+def exportCsv(locations, filePath = "export.csv"):
+    oFile = open(filePath, "wb")
+    writer = csv.writer(oFile,delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+    writer.writerow(["Step","latitude","longitude"])
+
+    for x in xrange(len(locations)):
+        location = locations[x]
+        writer.writerow([x,location.latitude,location.longitude])
+
+    oFile.close()
+
+
