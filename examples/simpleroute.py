98626e16720b466cc36a6fb3396f2bd7f2f6cc78
XST <dadelaricaar@gmail.com>
2016-07-24 01:59:02 +0200

    Example

--- examples/simpleroute.py
+++ examples/simpleroute.py
@@ -19,3 +19,60 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+from route import Route
+from direction import Direction
+from location import Location
+from routemanager import RouteManager
+from utils import *
+import json
+
+
+def as_direction(dct):
+    return Direction(Location(dct['latitude'],dct['longitude']), dct['speed'])
+
+def main():
+    route = Route() # Creamos un objeto ruta
+
+    # JSON con vectores de pruebas
+    jDirections = [
+        '{"latitude": 41.967782, "longitude": 2.837736, "speed": 1.3}',
+        '{"latitude": 41.967691, "longitude": 2.837481, "speed": 0.1}',
+        '{"latitude": 41.967657, "longitude": 2.837486, "speed": 1}',
+        '{"latitude": 41.967175, "longitude": 2.836808, "speed": 1.3}',
+        '{"latitude": 41.967418, "longitude": 2.836306, "speed": 0}'
+    ]
+
+    # Anadimos a la ruta
+    for x in xrange(len(jDirections)):
+        direction = json.loads(jDirections[x], object_hook = as_direction)
+        route.addDirection(direction)
+
+    # Creamos un manager con la ruta
+    routeMgr = RouteManager(route)
+
+    # Generamos las ubicaciones exportandolas a kml y csv.
+    locations = routeMgr.applyEffects()
+    exportXml(locations, "std-test.kml")
+    exportCsv(locations, "std-test.csv")
+
+    # Anadimos multiplicador de pasos y exportamos
+    routeMgr.stepMultiplier = 4
+    locations = routeMgr.applyEffects()
+    exportXml(locations, "step-test.kml")
+    exportCsv(locations, "step-test.csv")
+
+    # Anadimos ruido y exportamos
+    routeMgr.noise = 20
+    locations = routeMgr.applyEffects()
+    exportXml(locations, "step-noise-test.kml")
+    exportCsv(locations, "step-noise-test.csv")
+
+    # Quitamos multiplicador y dejamos solo ruido
+    routeMgr.stepMultiplier = 0
+    locations = routeMgr.applyEffects()
+    exportXml(locations, "noise-test.kml")
+    exportCsv(locations, "noise-test.csv")
+
+if __name__ == "__main__":
+    main()
